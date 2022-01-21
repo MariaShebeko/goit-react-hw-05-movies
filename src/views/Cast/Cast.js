8012bbb15915e9797d221aa3c72165c51f4867e6
MariaShebeko <marygro89@gmail.com>
2022-01-21 08:43:14 +0200

    added extra styles to Cast cards

--- src/views/Cast/Cast.js
+++ src/views/Cast/Cast.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import api from '../../services/movies-api';
-import noImage from '../../images/no-image.webp';
+import noImage from '../../images/No-Image.png';
 import s from './Cast.module.css';
 
 export default function Cast() {
@@ -17,9 +17,30 @@ export default function Cast() {
       <div className={s.Wrapper}>
         {actors.length > 0 ? (
           <ul className={s.list}>
-            {actors.map(actor => (
-              <li key={actor.id} className={s.item}>
-                <img
+            {actors.map(actor => {
+              const imageUrl = `https://image.tmdb.org/t/p/w500${actor.profile_path}`;
+              return (
+                <li key={actor.id} className={s.item}>
+                  <div className={s.container}>
+                    <div
+                      className={s.card}
+                      style={{
+                        backgroundImage: actor.profile_path
+                          ? 'url(' + imageUrl + ')'
+                          : 'url(' + noImage + ')',
+                        // backgroundSize: '300px',
+                        // '&:hover': {
+                        //   background:
+                        //     'url(' + imageUrl + ') left center no-repeat',
+                        // },
+                      }}
+                    >
+                      <div className={s.border}>
+                        <h2 className={s.cardTitle}>{actor.name}</h2>
+                      </div>
+                    </div>
+                  </div>
+                  {/* <img
                   src={
                     actor.profile_path
                       ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
@@ -28,9 +49,10 @@ export default function Cast() {
                   alt={actor.name}
                   className={s.image}
                 />
-                <p className={s.text}>{actor.name}</p>
-              </li>
-            ))}
+                <p className={s.text}>{actor.name}</p> */}
+                </li>
+              );
+            })}
           </ul>
         ) : (
           <p>There is no information about cast of this movie</p>
